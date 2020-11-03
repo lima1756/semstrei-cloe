294b2ac3b5a260388108868daed34b59c26355aa
Ivan Morett <luisivanmorett@gmail.com>
2020-11-03 16:15:41 -0600

    todo

--- api/app/routes/gen_otb.py
+++ api/app/routes/gen_otb.py
@@ -5,6 +5,10 @@ password_recovery_blueprint = Blueprint('passsword_recovery', __name__)
 
 class GenOTB(MethodView):
     def get(self):
+        #TODO:
+        # - query de la bdd para obtener la nueva info
+        # - llamar codigo de Alex para calcular el OTB
+        # - hacer uso de modelos creado por Alex y guardar los nuevos datos
         pass
 
 
