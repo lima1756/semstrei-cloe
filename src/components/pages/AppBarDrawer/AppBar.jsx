d047b2640443ad1b26d0c0c079788cd3068c3fce
Ivan Morett <luisivanmorett@gmail.com>
2020-11-26 15:48:22 -0600

    logout request

--- src/components/pages/AppBarDrawer/AppBar.jsx
+++ src/components/pages/AppBarDrawer/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { 
+import {
   AppBar, Button, Toolbar, Typography,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { userinformation, signin } from '../../../redux/actions';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 //import logo from '../assets/cloeLogo.webp';
 
 const useStyles = makeStyles((theme) => ({
@@ -25,26 +26,31 @@ export default function ElevateAppBar(props) {
   const dispatch = useDispatch();
 
   const handleCloseAccount = () => {
+    axios.post('https://150.136.172.48/api/auth/logout', {}, {
+      headers: {
+        'Authorization': `Bearer ${isLogged.token}`
+      }
+    })
     dispatch(signin(false, ''));
-    dispatch(userinformation(false, false, '','','',-1,-1));
+    dispatch(userinformation(false, false, '', '', '', -1, -1));
     history.push('/login');
   };
 
   return (
     <div className={classes.root}>
-        <AppBar position="static" style={{background: '#000'}}>
-          <Toolbar>
-            <Typography className={classes.title} variant="h6" align='left'>
-              Cloe
+      <AppBar position="static" style={{ background: '#000' }}>
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" align='left'>
+            Cloe
             </Typography>
-            {
-              isLogged.logged ?
+          {
+            isLogged.logged ?
               <Button color="inherit" variant='outlined' onClick={handleCloseAccount}>Cerrar sesión</Button>
               :
               null
-            }
-          </Toolbar>
-        </AppBar>
+          }
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
