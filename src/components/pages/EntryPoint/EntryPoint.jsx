fe56717626d91d03aa8d48ec4d6da4181eb6c87a
Ivan Morett <luisivanmorett@gmail.com>
2020-12-09 18:29:17 -0600

    refresh token

--- src/components/pages/EntryPoint/EntryPoint.jsx
+++ src/components/pages/EntryPoint/EntryPoint.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import axios from 'axios';
 import https from 'https';
+import { useHistory } from 'react-router-dom';
 import { signin } from '../../../redux/actions';
 
 
@@ -11,7 +15,16 @@ const httpsAgent = new https.Agent({
 })
 axios.defaults.options = httpsAgent
 
+const useStyles = makeStyles((theme) => ({
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+}));
+
 export default function EntryPoint() {
+    const classes = useStyles();
+    const history = useHistory();
     const isLogged = useSelector(state => state.logged);
     const dispatch = useDispatch();
 
@@ -23,12 +36,16 @@ export default function EntryPoint() {
         }).then(function (response) {
             let status = response.data.status === 'success' ? true : false;
             dispatch(signin(status, response.data.auth_token));
+            history.push('/dashboard/resultTables');
         }).catch(function (error) {
             if (error.response && error.response.status === 401) {
                 dispatch(signin(false, ''));
             }
+            history.push('/login');
         })
-        return <Redirect to={{ pathname: '/dashboard/resultTables' }} />
+        return <Backdrop className={classes.backdrop} open={true} >
+            <CircularProgress color="inherit" />
+        </Backdrop>
     }
     else {
         return <Redirect to={{ pathname: '/login' }} />
